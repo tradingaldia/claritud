ea2dbd08f48a64b19b178b7c21a6bb97e8d1462f
tradingaldia <datatradingaldia@gmail.com>
2026-03-06 14:45:59 -0500

    Create RootLayout component with metadata

---
+++ apps/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Claritud",
+  description: "Claritud Score"
+};
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
